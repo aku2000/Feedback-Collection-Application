2fd76b9fa8f40433b164ebcc62feb5adf5c13ded
Akansha Agarwal <akurox123@gmail.com>
2020-11-27 00:50:26 +0530

    add Google OAuth

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@
 makes use of es 2015 modules which is not supported by nodejs so in the backend part we will make use of below format that is common js modules(only in server side).*/
 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oath20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) =>
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callbacks'
+}, (accessToken) =>
 {
-    res.send({ bye: 'Guddi' });
-});
+    console.log(accessToken);
+})
+);
 
 //Dynamic Port Binding using Heroku
 const PORT = process.env.PORT || 5000;
