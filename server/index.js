238a771870b58cf5d0789bab5ee6169d5dcec89e
Akansha Agarwal <akurox123@gmail.com>
2020-11-27 12:23:00 +0530

    install nodemon

--- server/index.js
+++ server/index.js
@@ -15,9 +15,11 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
         },
-        accessToken =>
+        (accessToken, refreshToken, profile, done) =>
         {
-            console.log(accessToken);
+            console.log('access Token', accessToken);
+            console.log('refresh Token', refreshToken);
+            console.log('profile:', profile);
         }
     )
 );
