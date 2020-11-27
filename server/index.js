5e2609b5836ae33de46cce1f4dd21f6af340137e
Akansha Agarwal <akurox123@gmail.com>
2020-11-27 21:58:50 +0530

    install mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 /* import express from 'express' 
 makes use of es 2015 modules which is not supported by nodejs so in the backend part we will make use of below format that is common js modules(only in server side).*/
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 require('./services/passport');
+
+mongoose.connnect(keys.mongoURI);
 
 const app = express();
 
