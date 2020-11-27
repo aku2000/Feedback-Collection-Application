a13ce8199026ca78697c9bbf37c7172382ff5c77
Akansha Agarwal <akurox123@gmail.com>
2020-11-27 12:04:00 +0530

    add route-handler

--- server/index.js
+++ server/index.js
@@ -3,20 +3,30 @@ makes use of es 2015 modules which is not supported by nodejs so in the backend 
 
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oath20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callbacks'
-}, (accessToken) =>
-{
-    console.log(accessToken);
-})
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken =>
+        {
+            console.log(accessToken);
+        }
+    )
 );
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //Dynamic Port Binding using Heroku
 const PORT = process.env.PORT || 5000;
