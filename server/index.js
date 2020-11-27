ed0da9d550d38845d34e9c51ba4201d08a8bac11
Akansha Agarwal <akurox123@gmail.com>
2020-11-27 12:49:06 +0530

    refactor server directory

--- server/index.js
+++ server/index.js
@@ -1,34 +1,11 @@
 /* import express from 'express' 
 makes use of es 2015 modules which is not supported by nodejs so in the backend part we will make use of below format that is common js modules(only in server side).*/
-
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        (accessToken, refreshToken, profile, done) =>
-        {
-            console.log('access Token', accessToken);
-            console.log('refresh Token', refreshToken);
-            console.log('profile:', profile);
-        }
-    )
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 //Dynamic Port Binding using Heroku
 const PORT = process.env.PORT || 5000;
