6c23860b7b701237e460943fbfe3890d85232a3f
Akansha Agarwal <akurox123@gmail.com>
2020-11-29 15:31:49 +0530

    fix Proxy error

---
+++ server/client/src/setupProxy.js
@@ -0,0 +1,8 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
+module.exports = function (app)
+{
+    app.use(createProxyMiddleware('/auth/google', // replace with your endpoint
+        { target: 'http://localhost:5000' } // replace with your target
+    ));
+}
