0a0dac780b3975339012a9cb131eab21918bc337
Akansha Agarwal <akurox123@gmail.com>
2020-11-30 23:22:08 +0530

    refactor code

--- server/services/passport.js
+++ server/services/passport.js
@@ -34,14 +34,11 @@ passport.use(
             const existingUser = await User.findOne({ googleId: profile.id })
             if (existingUser)
             {
-                done(null, existingUser);
+                return done(null, existingUser);
             }
-            else
-            {
-                //create mongo model instance aka new record
-                const user = await new User({ googleId: profile.id }).save()
-                done(null, user);
-            }
+            //create mongo model instance aka new record
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
         }
     )
 );
