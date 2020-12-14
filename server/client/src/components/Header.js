f230d49cf95aa08c7da00c9acb6938c6710b5dcc
Akansha Agarwal <akurox123@gmail.com>
2020-12-15 01:59:43 +0530

    add navbar

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,9 +5,17 @@ class Header extends Component
     render()
     {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="left brand-logo">
+                        Feedback Hut
+                        <img src="../images/Logo.png" height="auto" width="auto" />
+                    </a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#">Login with Google</a></li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
