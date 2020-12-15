01c78b1c527babc54b2fe2d834f4e7ff5718000b
Akansha Agarwal <akurox123@gmail.com>
2020-12-15 19:54:28 +0530

    refactor fetchUser action creator

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,12 +1,8 @@
 import axios from 'axios'
 import { FETCH_USER } from './types'
 
-export const fetchUser = () =>
+export const fetchUser = () => async dispatch =>
 {
-    return function (dispatch)
-    {
-        axios
-            .get('/api/current_user')
-            .then(res => dispatch({ type: FETCH_USER, payload: res }));
-    }
+    const res = await axios.get('/api/current_user')
+    dispatch({ type: FETCH_USER, payload: res });
 };
