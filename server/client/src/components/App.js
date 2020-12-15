9a31a223fd1e9d5368112c7d8a2ed7b5a707db3a
Akansha Agarwal <akurox123@gmail.com>
2020-12-15 19:42:26 +0530

    add action creator in App Component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header'
 
 const Dashboard = () => <h2>Dashboard</h2>
@@ -10,7 +13,7 @@ class App extends Component
 {
     componentDidMount()
     {
-
+        this.props.fetchUser();
     }
     render()
     {
@@ -29,4 +32,4 @@ class App extends Component
     }
 }
 
-export default App;
+export default connect(null, actions)(App);
